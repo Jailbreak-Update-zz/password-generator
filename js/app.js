1aaf15beb46c360855f05cb986ef89b032d05535
Jailbreak Updates <78814920+Jailbreak-Update@users.noreply.github.com>
2022-02-06 23:45:27 -0600

    update

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@
 	// Caracteres separados por categoria (propiedad del objeto).
 	var caracteres = {
 		numeros: '0 1 2 3 4 5 6 7 8 9',
-		simbolos: '! @ # $ % ^ & * ( ) _ - + = { [ } ] ; : < , > . ? /',
+		simbolos: '! ¡ @ # $ % ^ & * ( ) _ - + = { [ } ] ; : < , > . ? /',
 		mayusculas: 'A B C D E F G H I J K L M N O P Q R S T U V W X Y Z',
 		minusculas: 'a b c d e f g h i j k l m n o p q r s t u v w x y z'
 	}
@@ -49,7 +49,7 @@
 	// Evento para el boton que decrementa en uno el numero de caracteres.
 	app.elements.namedItem('btn-menos-uno').addEventListener('click', function(){
 		// Validamos que no puedan seleccionar un numero menor a 1
-		if (configuracion.caracteres > 1) {
+		if (configuracion.caracteres > 8) {
 			configuracion.caracteres--;
 			inputCaracteres.value = configuracion.caracteres;
 
